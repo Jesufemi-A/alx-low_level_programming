153c7d988f7b5be7d1dae1d791f8332e24c697b1
Jesufemi-A <jesufemia1@gmail.com>
2023-02-16 14:39:29 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - A program that returns a string
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts(""Programming is like building a multilingual puzzle");
-	return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
 }
