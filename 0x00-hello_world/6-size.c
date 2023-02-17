0d430d57e47794c29fb122b01e1cd52583f93785
Jesufemi-A <jesufemia1@gmail.com>
2023-02-16 16:07:02 -0800

    Size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of data types
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)", sizeof(char));
+printf("Size of an int: %lu bytes(s)", sizeof(int));
+printf("Size of a long int: %lu  byte(s)", sizeof(long));
+printf("Size of a long long int: %lu byte(s)", sizeof(long long));
+printf("Size of a float: %lu byte(s)", sizeof (float));
+return (0);
+}
