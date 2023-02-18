03f0aa4f2fb66eee06f017f978f3e6f5a0b11403
Jesufemi-A <jesufemia1@gmail.com>
2023-02-18 14:01:03 -0800

    lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,17 @@
 
 /*
  * main - prints lowercase alphabets
- * followed by a new line
- * Return : Always 0 success
- *
+ * Return : Always (0) success
  */
 
 int main(void)
 {
-	char lower_case_alphabet = 'a';
+	char lower_case_alphabet;
 
-	for (lower_case_alphabet; lower_case_alphabet <= z; lower_case_alphabet++)
+	for (lower_case_alphabet = a; lower_case_alphabet <= z; lower_case_alphabet++)
 	{
 		putchar(lower_case_alphabet);
 	}
-	putchar(26);
-		return (0);
+	putchar(10);
+	return (0);
 }
