de3bcc0bea0438034397357e53d61e2c2fb58555
Jesufemi-A <jesufemia1@gmail.com>
2023-02-20 13:44:37 -0800

    combo

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,32 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
- *
- * Descrption: Print
- *
- * Return: Always 0 (Succes)
- *
+ * main - Program entry point
+ * Return: 0 on success, Error code otherwise
  */
 int main(void)
 {
-        int p, q;
-        for (p = 0; p <= 98; p++)
-        {
-                for (q = p + 1; q <= 99; q++)
-                {
-                        putchar((p / 10) + '0');
-                        putchar((p % 10) + '0');
-                        putchar (' ');
-                        putchar((q / 10) + '0');
-                        putchar((q % 10) + '0');
-                        if (p == 98 && q == 99)
-                                continue;
-                        putchar (',');
-                        putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+	int i, j;
+
+		for (i = 0; i < 100; i++)
+		{
+			for (j = 0; j < 100; j++)
+			{
+				if (i < j)
+				{
+					putchar((i / 10) + 48);
+					putchar((i % 10) + 48);
+					putchar(' ');
+					putchar((j / 10) + 48);
+					putchar((j % 10) + 48);
+					if (i != 98 || j != 99)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+		putchar ('\n');
+		return (0);
 }
