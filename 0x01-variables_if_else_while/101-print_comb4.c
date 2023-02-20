55eb39dc835c40eaa4b49bc8a779037d1a0923f7
Jesufemi-A <jesufemia1@gmail.com>
2023-02-20 13:48:04 -0800

    combination

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,36 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+
 /**
- * main - Entry point
- *
- * Description: prints all possible different combinations of three digits
- *
- * Return: Always 0 (success)
- *
+ * main - Print the numbers of 000 to 999.
+ * Return: 0
  */
+
 int main(void)
 {
-        int d, p, q;
-        for (d = '0'; d < '9'; d++)
-        {
-                for (p = d + 1; p <= '9'; p++)
-                {
-                        for (q = p + 1; q <= '9'; q++)
-                        {
-                                if ((p != d) != q)
-                                {
-                                putchar(d);
-                                putchar(p);
-                                putchar(q);
-                                if (d == '7' && p == '8')
-                                continue;
-                                putchar(',');
-                                putchar(' ');
-                                }
-                                }
-                                }
-                                }
-                                putchar('\n');
-                                return (0);
+	int num1, num2, num3;
+
+	for (num1 = '0' ; num1 <= '9' ; num1++)
+	{
+		num2 = num1 + 1;
+		for (; num2 <= '9' ; num2++)
+		{
+			num3 = num2 + 1;
+			for (; num3 <= '9'; num3++)
+			{
+				putchar(num1);
+				putchar(num2);
+				putchar(num3);
+				if (num1 == '7' && num2 == '8' && num3 == '9')
+				{
+					break;
+				}
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar(10);
+	return (0);
 }
