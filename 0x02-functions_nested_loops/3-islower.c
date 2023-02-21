82849012fca3742c4701ec83cb40e4c4f5b7f7e6
Jesufemi-A <jesufemia1@gmail.com>
2023-02-21 11:13:21 -0800

    lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,18 +2,14 @@
 /**
  * _islower - function that checks for lowercase character
  * @c: checks for lower alphabets
- * Return: Always 0 (Success)
+ * Description: checks for lower case
+ * Return: 1 or 0
  */
 
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
-	{
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
-
 }
