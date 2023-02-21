3429e737feb6ee67b2e40637625c23a5c3e1ddc1
Jesufemi-A <jesufemia1@gmail.com>
2023-02-21 03:49:53 -0800

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	putchar('\n');
+	return (0);
+
+}
