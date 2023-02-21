c510873ba7bcfa2c0e963d4a07c1e20e3a416562
Jesufemi-A <jesufemia1@gmail.com>
2023-02-21 06:34:29 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+
+void print_alphabet(void)
+{
+	int n;
+	char alpha;
+
+	for (n = 1; n <= 26; n++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+			_putchar(alpha);
+	}
+	putchar('\n');
+}
