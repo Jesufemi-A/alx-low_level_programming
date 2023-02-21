211345ac6bb1c0f9c20e51fdb164e6addad34a5e
Jesufemi-A <jesufemia1@gmail.com>
2023-02-21 15:39:28 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase,
+ * followed by a new line
+ * Return: Alwys 0 (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	char alph;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		alph = 'a';
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+					alph++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
