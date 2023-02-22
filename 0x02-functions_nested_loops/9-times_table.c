9394a395f622b576fdf7e9e05af37fd49c11e210
Jesufemi-A <jesufemia1@gmail.com>
2023-02-21 18:01:17 -0800

    add

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void times_table(void)
+{
+	int n;
+	for ( n = 0; n <= 9; n++)
+	{
+		_putchar(0);
+		_putchar(',');
+		_putchar(' ');
+		_putchar('\n');
+	}
+}
