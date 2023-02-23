550b1fbf411d91fb2fd186eec507faf8c9847332
Jesufemi-A <jesufemia1@gmail.com>
2023-02-23 08:35:14 -0800

    testing 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * mul - a function that multiplies two integers
+ * @a: recieves parameter
+ * @b: collects values
+ * Return: value of product
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - displays the value from 0 to 9
+ * Return: displays digit value
+ */
+
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a >= 0; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
