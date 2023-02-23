a1ee47e828370b8a339074d2f3299eb87fbdcf59
Jesufemi-A <jesufemia1@gmail.com>
2023-02-23 05:12:27 -0800

    testing

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - function that checks for uppercase character
+ * @c: formal parameter
+ * Return: 1 is true or 0 if false
+ */
+
+
+int _isupper(int c)
+{
+	if (_isupper(c))
+		return (1);
+	else
+		return (0);
+}
