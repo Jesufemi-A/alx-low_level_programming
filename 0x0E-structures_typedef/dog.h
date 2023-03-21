da221254140f552d49f53ff1c2c57da8ef63f0d9
Jesufemi-A <jesufemia1@gmail.com>
2023-03-21 07:46:15 -0700

    struct mem

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - collection of non-homogenous variables
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+#endif
