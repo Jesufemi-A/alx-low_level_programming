376a0bfcf62fce6e696db754f68b2c9e95a9b706
Jesufemi-A <jesufemia1@gmail.com>
2023-04-03 15:08:41 -0700

    number of node-elements

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -3,8 +3,20 @@
 #include <stdlib.h>
 
 /**
- *
+ * listint_len - sums the length of the element
+ * @h: head pointer parameter
+ * Return: nunber of element in linked list
  *
  */
 
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
+{
+	int num = 0;
+
+	while (h)
+	{
+		num++;
+		h = h->next;
+	}
+	return (num);
+}
--- 0x13-more_singly_linked_lists/1-main.c
+++ 0x13-more_singly_linked_lists/1-main.c
@@ -1,22 +1,32 @@
-#include "lists.h"
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
 
 /**
- * listint_len - function that returns the number of elements in a linked list
- * @h: head pointer parameter
- * Return: the number of elements in list
- */
-
-size_t listint_len(const listint_t *h)
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     */
+int main(void)
 {
-	int num = 0;
-	listint_t *traveller = h;
+	    listint_t *head;
+	        listint_t *new;
+		    listint_t hello = {8, NULL};
+		        size_t n;
 
-	while (traveller)
-	{
-		num++;
-		traveller = traveller->next
-	}
-	return (num);
+			    head = &hello;
+			        new = malloc(sizeof(listint_t));
+				    if (new == NULL)
+					        {
+							        printf("Error\n");
+								        return (1);
+									    }
+				        new->n = 9;
+					    new->next = head;
+					        head = new;
+						    n = listint_len(head);
+						        printf("-> %lu elements\n", n);
+							    free(new);
+							        return (0);
 }
