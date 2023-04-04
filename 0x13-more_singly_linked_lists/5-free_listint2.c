25c82c88f12ef1f427de366260bfa9886c5b3377
Jesufemi-A <jesufemia1@gmail.com>
2023-04-04 07:38:37 -0700

    resolve free_head_pointer

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_listint2 - a function that frees a listint_t list
+ * @head: head double pointer
+ * Return: void
+ */
+
+
+void free_listint2(listint_t **head)
+{
+	listint_t *help;
+
+	help = *head;
+
+	while (help != NULL)
+	{
+		help = help->next;
+		free(*head);
+		*head = help;
+	}
+}
