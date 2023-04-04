fb6b587c022f441bd2a505e5db4ddf8abb1db13a
Jesufemi-A <jesufemia1@gmail.com>
2023-04-04 11:24:43 -0700

    add new_node at nth index

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,7 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		length++;
 		help = help->next;
 	}
-	if (idx > length - 1)
+	if (idx > length)
 		return (NULL);
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
