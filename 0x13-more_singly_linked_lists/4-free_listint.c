289a4c8f607c425904a7b57141dc7129b7a9c615
Jesufemi-A <jesufemia1@gmail.com>
2023-04-04 00:02:03 -0700

    delete file

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,8 +3,21 @@
 #include <stdlib.h>
 
 /**
- *
- *
+ * free_listint - frees the list
+ * @head: head pointer to the first node
+ * Return: returns nothing
  */
+
+void free_listint(listint_t *head)
+{
+	listint_t *help = head;
+
+	while (help)
+	{
+		help = head->next;
+		free = (head);
+		head = help;
+	}
+}
 
 
