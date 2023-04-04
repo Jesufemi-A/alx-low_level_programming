26d33bf61834314167f60213e57f63b6320d0e72
Jesufemi-A <jesufemia1@gmail.com>
2023-04-04 11:19:15 -0700

    add new_node at nth index

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - insert a new node at idx index
+ * @head: double head pointer position
+ * @idx: index of new node
+ * @n: data of new node
+ * Return: the newnode
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i, length;
+	listint_t *help, *traveller, *new_node;
+
+	length = 0;
+	help = *head;
+	traveller = *head;
+	while (help)
+	{
+		length++;
+		help = help->next;
+	}
+	if (idx > length - 1)
+		return (NULL);
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+		return (NULL);
+	help = *head;
+	new_node->n = n;
+	for (i = 0; i <= idx; i++)
+		traveller = traveller->next;
+	for (i = 0; i < idx; i++)
+		help = help->next;
+	help->next = new_node;
+	new_node->next = traveller;
+	return (new_node);
+}
