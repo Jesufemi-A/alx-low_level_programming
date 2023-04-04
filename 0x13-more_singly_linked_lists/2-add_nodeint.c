1c1b537aaff636585c6727ec7bb61f6225e5ccfb
Jesufemi-A <jesufemia1@gmail.com>
2023-04-04 06:29:07 -0700

    reslove add_node

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,18 +14,12 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
 
-	if (*head == NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(listint_t));
 
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
-	}
-	else
+	if (new_node == NULL)
 		return (NULL);
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
